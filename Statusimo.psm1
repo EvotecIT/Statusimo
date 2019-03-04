f11eb87a70cc7d6c280e670dfb10be05282b498f
PrzemyslawKlys <pklys@evotec.pl>
2019-03-04 20:46:23 +0100

    1st commit

---
+++ Statusimo.psm1
@@ -0,0 +1,22 @@
+#Get public and private function definition files.
+$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue -Recurse )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue -Recurse )
+$Assembly = @( Get-ChildItem -Path $PSScriptRoot\Lib\*.dll -ErrorAction SilentlyContinue -Recurse )
+
+#Dot source the files
+Foreach ($import in @($Public + $Private)) {
+    Try {
+        . $import.fullname
+    } Catch {
+        Write-Error -Message "Failed to import function $($import.fullname): $_"
+    }
+}
+Foreach ($import in @($Assembly)) {
+    Try {
+        Add-Type -Path $import.fullname
+    } Catch {
+        Write-Error -Message "Failed to import DLL $($import.fullname): $_"
+    }
+}
+
+Export-ModuleMember -Function '*'
