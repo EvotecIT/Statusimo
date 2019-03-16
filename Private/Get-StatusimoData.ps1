28d34f4d7d72bcf557482b8363a830dc312068c0
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-03-16 20:09:35 +0100

    Fix for TimeZone problem

--- Private/Get-StatusimoData.ps1
+++ Private/Get-StatusimoData.ps1
@@ -6,7 +6,11 @@ function Get-StatusimoData {
     if ($FolderPath -ne '' -and (Test-Path -LiteralPath $FolderPath)) {
         $Files = Get-ChildItem -LiteralPath $FolderPath    
         foreach ($File in $Files) {       
-            $Output = Get-Content -LiteralPath $File.FullName -Raw | ConvertFrom-Json
+            $Output = Get-Content -LiteralPath $File.FullName | ConvertFrom-Json      
+            $TimeZoneBias = (Get-CimInstance -ClassName Win32_TimeZone).Bias
+            $Output.PSObject.Properties | Where-Object {$_.Name -like 'Date*' } | ForEach-Object {
+                $Output.($_.Name) = $_.Value.AddMinutes($TimeZoneBias)
+            }
             Add-Member -InputObject $Output -Name 'FullName' -Value $File.FullName -MemberType NoteProperty -Force
             $Output
         }
