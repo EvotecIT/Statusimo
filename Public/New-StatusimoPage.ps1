e71bfa7bf5670b087438100edb6139e2cc0b048e
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-08-02 22:03:43 +0200

    Support for new PSWriteHTML

--- Public/New-StatusimoPage.ps1
+++ Public/New-StatusimoPage.ps1
@@ -14,91 +14,72 @@ function New-StatusimoPage {
 
         $IncidentTypes = $Incidents.Service | Sort-Object -Unique
 
-        New-HTMLPanel -Invisible {
-            $Statuses = foreach ($Type in $IncidentTypes) {
-                $Incidents | Where-Object { $_.Service -eq $Type } | Select-Object -First 1 -ExpandProperty Status
-            }
-            if ($Statuses -notcontains 'Partial Degradation' -and $Statuses -notcontains 'Down') {
-                New-HTMLToast -Icon Information -Color Blue -TextHeader 'Information' -Text 'Everything is running smoothly!'
-            } else {
-                New-HTMLToast -Icon Exclamation -Color Orange -TextHeader 'Warning' -Text "Some systems are affected. We're hard at work fixing."
-            }
-        }
-
-        New-HTMLHeading -Heading h1 -HeadingText 'Current Status' -Type 'central' -Color Black
-
-        New-HTMLPanel -Invisible {
-            New-HTMLStatus {
-
-                foreach ($Type in $IncidentTypes) {
-                    $IncidentStatus = $Incidents | Where-Object { $_.Service -eq $Type } | Select-Object -First 1
-                    if ($IncidentStatus.Status -eq 'Operational') {
-                        $Icon = 'Good'
-                        $Percentage = '100%'
-                    } elseif ($IncidentStatus.Status -eq 'Partial Degradation') {
-                        $Icon = 'Bad'
-                        $Percentage = '30%'
-                    } elseif ($IncidentStatus.Status -eq 'Down') {
-                        $Icon = 'Dead'
-                        $Percentage = '0%'
-                    }
-
-                    New-HTMLStatusItem -ServiceName $IncidentStatus.Service -ServiceStatus $IncidentStatus.Status -Icon $Icon -Percentage $Percentage
+        New-HTMLSection -Invisible {
+            New-HTMLContainer -Width '900px' -Margin 'auto' {
+                $Statuses = foreach ($Type in $IncidentTypes) {
+                    $Incidents | Where-Object { $_.Service -eq $Type } | Select-Object -First 1 -ExpandProperty Status
                 }
-            }
-        }
-
-        New-HTMLHeading -Heading h1 -HeadingText 'Scheduled Maintenance' -Type 'central' -Color Black
-
-        New-HTMLPanel -Invisible {
-            foreach ($Maintenance in $Maintenances) {
-                $Title = "$($Maintenance.Title) ($($Maintenance.DateStart) - $($Maintenance.DateEnd))"
-                if ($Today -ge $Maintenance.DateStart -and $Today -le $Maintenance.DateEnd) {
-                    New-HTMLToast -Icon Exclamation -Color Orange -TextHeader $Title -Text $Maintenance.Overview
-                } elseif ($Today -le $Maintenance.DateStart) {
-                    New-HTMLToast -Icon Information -Color Blue -TextHeader $Title -Text $Maintenance.Overview
+                if ($Statuses -notcontains 'Partial Degradation' -and $Statuses -notcontains 'Down') {
+                    New-HTMLToast -TextHeader 'Information' -Text 'Everything is running smoothly!' -BarColorLeft Blue -IconSolid info-circle -IconColor Blue
                 } else {
-                    New-HTMLToast -Icon Success -Color Green -TextHeader $Title -Text $Maintenance.Overview
+                    New-HTMLToast -TextHeader 'Warning' -Text "Some systems are affected. We're hard at work fixing." -BarColorLeft Orange -IconSolid exclamation-circle -IconColor Orange
                 }
-            }
-        }
 
+                New-HTMLHeading -Heading h1 -HeadingText 'Current Status' -Color Black
 
-        New-HTMLHeading -Heading h1 -HeadingText 'Incidents per day' -Type 'central' -Color Black
+                New-HTMLStatus {
 
-        New-HTMLPanel -Invisible {
-            $Data = foreach ($Element in 30..0) {
-                $Date = (Get-Date).AddDays(-$Element).Date
-                $IncidentsPerDay = $Incidents | Where-Object { ($_.Status -eq 'Partial Degradation' -or $_.Status -eq 'Down') -and $_.Date.Date -eq $Date }
-                Get-ObjectCount -Object $IncidentsPerDay
-            }
-            $DataCategories = foreach ($Element in 30..0) {
-                (Get-Date).AddDays(-$Element).ToShortDateString()
-            }
-            <#
-            New-HTMLChart `
-                -Data $Data `
-                -DataNames $DataName `
-                -DataCategories $DataCategories `
-                -Type 'line' `
-                -TitleText '' `
-                -TitleAlignment 'left' `
-                -LineColor 'Blue' -Horizontal $false -Positioning central
+                    foreach ($Type in $IncidentTypes) {
+                        $IncidentStatus = $Incidents | Where-Object { $_.Service -eq $Type } | Select-Object -First 1
+                        if ($IncidentStatus.Status -eq 'Operational') {
+                            $Icon = 'Good'
+                            $Percentage = '100%'
+                        } elseif ($IncidentStatus.Status -eq 'Partial Degradation') {
+                            $Icon = 'Bad'
+                            $Percentage = '30%'
+                        } elseif ($IncidentStatus.Status -eq 'Down') {
+                            $Icon = 'Dead'
+                            $Percentage = '0%'
+                        }
 
-                #>
+                        New-HTMLStatusItem -ServiceName $IncidentStatus.Service -ServiceStatus $IncidentStatus.Status -Icon $Icon -Percentage $Percentage
+                    }
+                }
 
-            New-HTMLChart -Title 'Incidents per day' -TitleAlignment left -Positioning central {
-                New-ChartCategory -Name $DataCategories
-                New-ChartLine -Name 'Incidents' -Value $Data
-            }
-        }
+                New-HTMLHeading -Heading h1 -HeadingText 'Scheduled Maintenance' -Color Black
 
-        New-HTMLHeading -Heading h1 -HeadingText 'Timeline' -Type 'central' -Color Black
+                foreach ($Maintenance in $Maintenances) {
+                    $Title = "$($Maintenance.Title) ($($Maintenance.DateStart) - $($Maintenance.DateEnd))"
+                    if ($Today -ge $Maintenance.DateStart -and $Today -le $Maintenance.DateEnd) {
+                        New-HTMLToast -TextHeader $Title  -Text $Maintenance.Overview -BarColorLeft Orange -IconSolid exclamation-circle -IconColor Orange
+                    } elseif ($Today -le $Maintenance.DateStart) {
+                        New-HTMLToast -TextHeader $Title  -Text $Maintenance.Overview -BarColorLeft Blue -IconSolid info-circle
+                    } else {
+                        New-HTMLToast -TextHeader $Title -Text $Maintenance.Overview -BarColorLeft Green -IconSolid check-circle -IconColor Green
+                    }
+                }
 
-        New-HTMLPanel -Invisible {
-            New-HTMLTimeline {
-                foreach ($Incident in $Incidents) {
-                    New-HTMLTimelineItem -HeadingText $Incident.Title -Text $Incident.Overview -Date $Incident.Date -Color Black
+                New-HTMLHeading -Heading h1 -HeadingText 'Incidents per day' -Color Black
+
+                $Data = foreach ($Element in 30..0) {
+                    $Date = (Get-Date).AddDays(-$Element).Date
+                    [Array] $IncidentsPerDay = $Incidents | Where-Object { ($_.Status -eq 'Partial Degradation' -or $_.Status -eq 'Down') -and $_.Date.Date -eq $Date }
+                    $IncidentsPerDay.Count
+                }
+                $DataCategories = foreach ($Element in 30..0) {
+                    (Get-Date).AddDays(-$Element).ToShortDateString()
+                }
+                New-HTMLChart -Title 'Incidents per day' -TitleAlignment left {
+                    New-ChartCategory -Name $DataCategories
+                    New-ChartLine -Name 'Incidents' -Value $Data
+                }
+
+                New-HTMLHeading -Heading h1 -HeadingText 'Timeline' -Color Black
+
+                New-HTMLTimeline {
+                    foreach ($Incident in $Incidents) {
+                        New-HTMLTimelineItem -HeadingText $Incident.Title -Text $Incident.Overview -Date $Incident.Date -Color Black
+                    }
                 }
             }
         }
